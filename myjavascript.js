ed9716acc1fdcfabc08ccf3cc262a1cb7f968903
Ben Halverson <benhalverson33@gmail.com>
2014-09-09 22:04:27 -0700

    added javascript function

--- myjavascript.js
+++ myjavascript.js
@@ -1,1 +1,12 @@
-d
+$(document).ready(function() {
+    $('#toggle-child').click(function() {
+        $('.child-menu').slideToggle("fast");
+    });
+
+    function animateIcon(){
+        $('.number-icon').fadeIn(400).delay(400).fadeOut(400, animateIcon);
+    }
+    animateIcon();
+
+
+});
