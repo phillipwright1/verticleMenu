36e34d78f5e587795c4394f4004e44fb899fe477
phillip wright <bootsie8917@gmail.com>
2014-09-10 16:28:36 -0700

    added animations

--- myjavascript.js
+++ myjavascript.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
-    $('#toggle-child').click(function() {
-        $('.child-menu').toggle("fast");
+    $('.profile').click(function() {
+        $('.child-menu1').toggle("fast");
     });
+    $('.messages').click(function() {
+        $('.child-menu2').toggle("fast");
+    });
+    $('.settings').click(function() {
+        $('.child-menu3').toggle("fast");
+    });
+    $('.logout').click(function() {
+        $('.child-menu4').toggle("fast");
+    });
+
 
     function animateIcon(){
         $('.number-icon').fadeIn(400).delay(400).fadeOut(400, animateIcon);
